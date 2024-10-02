1aab320f35d10bd97c729b431227a35142d33763
jacopolegovini <legovini96@gmail.com>
2024-10-02 21:42:57 +0200

    exercise completed

--- merenda.js
+++ merenda.js
@@ -1,3 +1,14 @@
 function rimuoviDuplicati(arr) {
     // A voi!
+    const newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let element = arr[i];
+
+        if (!newArr.includes(element)) {
+            newArr.push(element)
+        }
+    };
+
+    return newArr;
 }
